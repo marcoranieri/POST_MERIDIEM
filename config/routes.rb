c9425a45677a65d966b5108931988b27291b4186
sonia keredine <sonia.keredine@hotmail.fr>
2018-04-25 19:25:04 +0200

    search-page-2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   end
 
   get "users/:id/matches", to: "matches#user_matches"
+  get "search", to: "pages#search"
   #  get "places/:id/matches", to: "matches#destroy"
 
 
