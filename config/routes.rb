9a9401c5f53396d21771dfae31e82937431ca2df
marco ranieri <mr.marcoranieri@gmail.com>
2018-04-27 15:27:38 +0200

    link matches navbar

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :matches, only: [ :create]
   end
 
-  get "users/:id/matches", to: "matches#user_matches"
+  get "users/:id/matches", to: "matches#user_matches", as: :user_matches
   get "search", to: "pages#search"
   #  get "places/:id/matches", to: "matches#destroy"
 
