688cc014dc4167c05c65bf557c6c1b8ec4a388d3
marco ranieri <mr.marcoranieri@gmail.com>
2018-04-27 12:01:54 +0200

    added reviews details

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -17,9 +17,6 @@ class MatchesController < ApplicationController
 
   def user_matches
     @user_matches = Match.where(user_id: current_user)
-
-    photo_reference =  @user_matches[0].place.google_data["photos"][0]["photo_reference"]
-    @url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=#{photo_reference}&key=AIzaSyBSy-fahBAu25vH9WsuZuUd_jS2P6trgp8"
   end
 
   def update
