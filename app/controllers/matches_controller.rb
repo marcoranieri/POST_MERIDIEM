ef309855a30d6f977f12ca054333f20fa855d539
marco ranieri <mr.marcoranieri@gmail.com>
2018-04-28 20:53:41 +0200

    Added SWIPE right -> Create MATCH

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,14 +1,24 @@
 class MatchesController < ApplicationController
 
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!
+
+
   def create
+   if params.has_key?("username")
+     userid = params["username"].to_i
+     placeid = params["place_id"].to_i
+     @match = Match.new(user_id: userid, place_id: placeid)
+     @match.save!
+   else
     @place = Place.find(params[:place_id])
     @match = Match.new(matches_params)
     @match.user = current_user
     @match.place = @place
     user_id = current_user.id
-
     @match.save
-    # if @match.save
+  end
+    # # if @match.save
     #   redirect_to "/users/#{user_id}/matches"
     # else
     #   redirect_to "/places"
